d880852c00f89a2a0a6d02fda030e24208e08044
Ho Young Kim <149951378+hy36370637@users.noreply.github.com>
2024-08-11 09:45:59 +0900

    클론할 때 새로운 디렉토리 이름 사용
    git clone 명령어에서 $dir_name 변수를 사용하여 새로운 디렉토리로 소스를 복제

--- build_emacs.sh
+++ build_emacs.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 # emacs packaging for macOS
 
+# 현재 날짜를 "YYYYMMDD" 형식으로 가져오기
+current_date=$(date +"%Y%m%d")
+
+# 디렉토리 이름 설정
+dir_name="emacs"
+if [ -d "$dir_name" ]; then
+  dir_name="emacs_$current_date"
+  echo "Existing directory found. Cloning into $dir_name..."
+else
+  echo "No existing directory found. Cloning into $dir_name..."
+fi
+
 # Emacs 소스코드 클론
-echo "Cloning Emacs repository..."
-git clone git://git.sv.gnu.org/emacs.git
+git clone git://git.sv.gnu.org/emacs.git "$dir_name"
 
 # Emacs 디렉토리로 이동
-cd emacs || { echo "Failed to enter the emacs directory"; exit 1; }
+cd "$dir_name" || { echo "Failed to enter the $dir_name directory"; exit 1; }
 
 # autogen 실행
 echo "Running autogen.sh..."
